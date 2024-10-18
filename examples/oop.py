18920370259a5724e14dbbbe6d9b5b5e86a4bf60
Iulia Chiriac <iulia.chiriac@yahoo.com>
2024-10-18 09:58:33 +0300

    Improve OOP example

--- examples/oop.py
+++ examples/oop.py
@@ -3,14 +3,47 @@ from datetime import date
 
 class Person:
     count = 0  # class variable
+    MIN_YEAR = 1900
 
     def __init__(self, name, date_of_birth):
         self.name = name  # instance variable
         self.date_of_birth = date_of_birth
-        Person.count += 1
+        self._increment_count()
+
+    @property
+    def date_of_birth(self):  # getter
+        return self._date_of_birth
+
+    @date_of_birth.setter
+    def date_of_birth(self, value):  # setter
+        if not (isinstance(value, date) and value.year >= self.MIN_YEAR):
+            raise ValueError("Invalid date of birth.")
+        self._date_of_birth = value
+
+    @property
+    def age(self):
+        return self.years_since(self.date_of_birth)
+
+    @classmethod
+    def _increment_count(cls):  # class method
+        cls.count += 1
+
+    @staticmethod
+    def years_since(date_start):  # static method
+        today = date.today()
+        years = today.year - date_start.year
+        if (date_start.month, date_start.day) > (today.month, today.day):
+            years -= 1
+        return years
 
     def greet(self, greeting):  # instance method
         print(f"{greeting.capitalize()}! I am {self.name}.")
+
+    def __str__(self):
+        return f"Person object ({self.name}; {self.date_of_birth})"
+
+    def __lt__(self, other):
+        return self.date_of_birth > other.date_of_birth
 
 
 if __name__ == "__main__":
@@ -20,7 +53,25 @@ if __name__ == "__main__":
     print(p1.name, p1.date_of_birth)
     print(p2.name, p2.date_of_birth)
 
+    try:
+        p2.date_of_birth = date(1884, 5, 1)
+    except ValueError as ex:
+        print(ex)
+    print("Date of birth is unchanged:", p2.date_of_birth)
+
+    p1.name = "Jane"  # set attribute
+    print(p1.name)  # get attribute
+    # del p1.name  # del attribute
+
     print(Person.count, p1.count, p1.count is Person.count)
 
     p1.greet("hi")
     p2.greet("hello")
+
+    print(p1.__dict__)
+    print(p1, str(p1), repr(p1))
+
+    print(f"{p1.name} is younger than {p2.name}:", p1 < p2)
+
+    print(Person.years_since(date(1918, 12, 1)))
+    print(f"{p1.name}'s age is:", p1.age)
